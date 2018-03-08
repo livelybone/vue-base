b2db238e24a93dc24dcf322d9d13d58857f0e381
livelybone <2631541504@qq.com>
2018-03-08 11:12:47 +0800

    edit router

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,57 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import AuthToken from 'extensions/auth-token'
 import HelloWorld from 'pages/HelloWorld'
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/sign-in',
+      name: 'SignIn',
+      component: HelloWorld
+    },
+    {
+      path: '/admin-sign-in',
+      name: 'AdminSignIn',
+      component: HelloWorld
+    },
+    {
+      path: '/client',
+      meta: {requireAuth: true},
+      children: []
+    },
+    {
+      path: '/admin',
+      meta: {requireAdminAuth: true},
+      children: []
     }
   ]
-})
+});
+
+router.beforeEach((to, fr, next) => {
+  if (to.matched.some(route => route.meta.requireAuth) && !Vue.prototype.$store.state.user.id) {
+    AuthToken.getUser().then(() => {
+      next()
+    }).catch(e => {
+      next({name: 'SignIn', redirect: to.fullPath});
+      Vue.prototype.snackBar.error('请先登录！');
+    })
+  } else if (to.matched.some(route => route.meta.requireAdminAuth) && !Vue.prototype.$store.state.admin.id) {
+    AuthToken.getAdminUser().then(() => {
+      next()
+    }).catch(e => {
+      next({name: 'AdminSignIn', redirect: to.fullPath});
+      Vue.prototype.snackBar.error('请先登录管理端！');
+    })
+  } else next();
+});
+
+export default router
