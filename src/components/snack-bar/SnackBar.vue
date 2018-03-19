198096fae516c3883fbc8d488bf7c1e207f06a4d
livelybone <2631541504@qq.com>
2018-03-19 18:12:32 +0800

    'edit'

--- src/components/snack-bar/SnackBar.vue
+++ src/components/snack-bar/SnackBar.vue
@@ -51,7 +51,6 @@
       error(msg) {
         this.color = this.colors.error || colors.error;
         this.open({color: this.color, msg: msg});
-        this.redirect(msg);
         return false;
       },
       warn(msg) {
@@ -69,14 +68,6 @@
       getMsg(msg) {
         if (typeof msg === 'string') return msg;
         if (msg.message) return msg.name + ': ' + msg.message
-      },
-      redirect(msg) {
-        let m = typeof msg === 'string' ? msg : msg.message;
-        if (m.indexOf('未登录') >= 0 && this.$route.path.indexOf('sign-in') < 0) {
-          const isAdmin = this.$route.path.indexOf('admin') >= 0;
-          this.$router.push(`${isAdmin ? '/admin' : ''}/sign-in?redirect=${encodeURIComponent(this.$route.fullPath)}`);
-          this.$store.commit(isAdmin ? 'admin' : 'user', {})
-        }
       }
     }
   }
