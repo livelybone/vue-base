3cf51eeb164d1105013bdbd357421e6cc5f840c3
livelybone <2631541504@qq.com>
2018-03-26 17:32:25 +0800

    edit Status

--- src/data/immutable-data/status.js
+++ src/data/immutable-data/status.js
@@ -1,7 +1,7 @@
 export default class Status {
-  static status = [
-    {name: '审核中', value: 'WAIT'},
-    {name: '审核通过', value: 'PASS'},
-    {name: '审核未通过', value: 'NOT_PASS'},
-  ];
+  static status = {
+    WAIT: {name: '审核中', value: 'WAIT'},
+    PASS: {name: '审核通过', value: 'PASS'},
+    NOT_PASS: {name: '审核未通过', value: 'NOT_PASS'},
+  };
 }
