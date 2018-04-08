20791adc3a9dacf0b2f8f25a81bd88682415485d
livelybone <2631541504@qq.com>
2018-04-08 09:57:04 +0800

    edit localStorage

--- src/utils/localStorage.js
+++ src/utils/localStorage.js
@@ -18,19 +18,42 @@ export default class LocalStorage {
     try {
       if (this.localStorageSupport) {
         localStorage.setItem(key, val)
+      } else {
+        Cookie.set(key, val)
       }
     } catch (e) {
       console.error(e);
       this.localStorageSupport = false;
-      Cookie.set(key, val)
+      if (val) Cookie.set(key, val);
+      else Cookie.del(key);
     }
   }
 
   get(key) {
-    if (this.localStorageSupport) {
-      return localStorage.getItem(key)
-    } else {
-      return Cookie.get(key)
+    try {
+      if (this.localStorageSupport) {
+        return localStorage.getItem(key)
+      } else {
+        return Cookie.get(key)
+      }
+    } catch (e) {
+      console.error(e);
+      this.localStorageSupport = false;
+      Cookie.get(key)
+    }
+  }
+
+  del(key) {
+    try {
+      if (this.localStorageSupport) {
+        localStorage.removeItem(key)
+      } else {
+        Cookie.del(key)
+      }
+    } catch (e) {
+      console.error(e);
+      this.localStorageSupport = false;
+      Cookie.del(key);
     }
   }
 
