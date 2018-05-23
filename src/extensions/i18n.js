b193644be0d6f73e606a6475e96e53cb88ea06ec
livelybone <2631541504@qq.com>
2018-05-23 15:36:56 +0800

    update i18n

--- src/extensions/i18n.js
+++ src/extensions/i18n.js
@@ -4,13 +4,17 @@ import VueI18n from 'vue-i18n'
 import { CN } from "assets/lang/CN";
 import { CNT } from "assets/lang/CN-T";
 
+const messages = {
+  CN,
+  CNT,
+};
+
+export const Langs = Object.keys(messages).map(key => ({name: messages[key]['__name__'], value: key}));
+
 export function initialI18n() {
   Vue.use(VueI18n);
   return new VueI18n({
     locale: 'CN',
-    messages: {
-      CN,
-      CNT,
-    },
+    messages,
   });
 }
