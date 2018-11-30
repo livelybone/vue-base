236c060adc253bf39d31f8469f3b6633661484ff
livelybone <2631541504@qq.com>
2018-11-30 11:56:22 +0800

    update i18n

--- src/App.vue
+++ src/App.vue
@@ -24,9 +24,6 @@ export default {
 @import "./css/common.scss";
 
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
--- src/extensions/Langs.js
+++ src/extensions/Langs.js
@@ -1,23 +1,44 @@
 /**
  * 语言国际化
  * */
-import { CN } from 'assets/lang/CN/index'
-import { CNT } from 'assets/lang/CNT/index'
+/* eslint-disable no-param-reassign */
+import LangMap from 'assets/lang/LangMap.json'
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
 
-const messages = {
-  CN,
-  CNT,
+const getResources = lang => import(`assets/lang/${lang}` /* webpackChunkName: "Lang-[request]" */)
+const browserLang = typeof window !== 'undefined' && navigator.language.toLowerCase() === 'zh-cn' ? 'zh-HANS' : 'en-US'
+const loadedLanguages = []
+
+export const Langs = Object.keys(LangMap)
+  .map(key => ({ name: LangMap[key], value: key }))
+
+function setI18nLanguage(i18n, lang) {
+  i18n.locale = lang
+  document.querySelector('html').setAttribute('lang', lang)
+  return lang
 }
 
-export const Langs = Object.keys(messages)
-  .map(key => ({ name: messages[key].lang_name, value: key }))
+export function loadLanguageAsync(i18n, lang) {
+  if (!LangMap[lang]) throw new Error(`The language '${lang}' has not been included in LangMap.json`)
+  if (!loadedLanguages.includes(lang)) {
+    return getResources(lang).then((msgs) => {
+      i18n.setLocaleMessage(lang, msgs.default)
+      loadedLanguages.push(lang)
+      return setI18nLanguage(i18n, lang)
+    })
+  }
+  if (i18n.locale === lang) return Promise.resolve(lang)
+  return Promise.resolve(setI18nLanguage(i18n, lang))
+}
 
-export function initialI18n() {
+export function initialI18n(lang) {
   Vue.use(VueI18n)
+
+  const locale = lang || browserLang
+
   return new VueI18n({
-    locale: 'CN',
-    messages,
+    locale,
+    messages: {},
   })
 }
