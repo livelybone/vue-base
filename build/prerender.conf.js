4196b8a5928201f1547ad8c7c6c8bc0f4a37040e
livelybone <2631541504@qq.com>
2019-01-08 17:35:30 +0800

    update prerender.conf.js

--- build/prerender.conf.js
+++ build/prerender.conf.js
@@ -1,10 +1,10 @@
 'use strict'
 const path = require('path')
 
-const routes = [ // 在这里编辑需要预渲染的页面的路由
-  { path: '/' },
-  { path: '/not-found' },
-]
+// 在这里编辑需要预渲染的页面的路由
+const rs = ['/']
+const routes = ['en', 'zh-hans', 'zh-hant']
+  .reduce((pre, lang) => pre.concat(rs.map(r => ({ path: `/${lang}${r}` }))), [])
 
 module.exports = {
   // For more: https://github.com/chrisvfritz/prerender-spa-plugin/tree/v3
