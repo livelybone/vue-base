dbaa10f0c74ec24ffd54b3b2425abbb22dd368fe
livelybone <2631541504@qq.com>
2019-03-12 20:59:10 +0800

    feat(main.js, entry-client.js): Wrap app, store as a create function

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import App from '@/App'
-import store from '@/data/store'
+import { createStore } from '@/data/store'
 import { initialExtensions } from '@/extensions/Extensions'
 import register from '@/global-register'
 import { createRouter } from '@/router'
@@ -19,17 +19,16 @@ Vue.config.productionTip = false
 const { i18n } = initialExtensions()
 register()
 
-/* eslint-disable no-new */
-const router = createRouter(i18n, store)
-const root = new Vue({
-  router,
-  store,
-  i18n,
-  render: h => h(App),
-})
-
-document.addEventListener('DOMContentLoaded', () => {
-  // prod: webpack HtmlWebpackPlugin 配置 {inject: 'head',chunksSortMode: 'dependency'}，
-  // 使得js代码被插入到了 head 标签，先于 body DOM 生成之前运行，因此使用 DOMContentLoaded 事件处理
-  root.$mount('#app')
-})
+export function createApp() {
+  // 创建 router 和 store 实例
+  const store = createStore()
+  const router = createRouter(i18n, store)
+  // 创建应用程序实例，将 router 和 store 注入
+  const app = new Vue({
+    router,
+    store,
+    i18n,
+    render: h => h(App),
+  })
+  return { app, router, store }
+}
