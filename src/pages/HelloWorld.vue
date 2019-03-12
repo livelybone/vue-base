199684d7faf3917928f15a0c3b9dafcbe7b776cf
livelybone <2631541504@qq.com>
2019-03-12 09:36:28 +0800

    feat: format

--- src/pages/HelloWorld.vue
+++ src/pages/HelloWorld.vue
@@ -13,9 +13,9 @@
       <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
       <br />
       <li>
-        <a href="http://vuejs-templates.github.io/webpack/" target="_blank"
-          >Docs for This Template</a
-        >
+        <a href="http://vuejs-templates.github.io/webpack/" target="_blank">
+          Docs for This Template
+        </a>
       </li>
     </ul>
     <h2>Ecosystem</h2>
